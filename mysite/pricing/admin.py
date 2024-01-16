4b3982b72b501074c49b75f6693c4cc28bd7e561
Kural18 <kuralarasan.c.che19@itbhu.ac.in>
2024-01-16 17:22:07 +0530

    added auto save option

--- mysite/pricing/admin.py
+++ mysite/pricing/admin.py
@@ -4,5 +4,12 @@ from .models import PricingConfiguration
 class PricingConfigurationAdmin(admin.ModelAdmin):
     list_display = ('param_name', 'is_active', 'created_by', 'created_at', 'modified_by', 'modified_at')
     search_fields = ['param_name']
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # Object is being created
+            obj.created_by = request.user
+        obj.modified_by = request.user
+
+        super().save_model(request, obj, form, change)
 
 admin.site.register(PricingConfiguration, PricingConfigurationAdmin)
